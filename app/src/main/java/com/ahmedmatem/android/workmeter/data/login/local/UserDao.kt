09b0272bb52bb666ab920fb96b9c987b6ebfae60
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2022-12-30 12:03:40 +0000

    Add User Dao

---
+++ app/src/main/java/com/ahmedmatem/android/workmeter/data/login/local/UserDao.kt
@@ -0,0 +1,19 @@
+package com.ahmedmatem.android.workmeter.data.login.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Query("Select * from users where username like :username and password like password limit 1")
+    fun getBy(username: String, password: String): User
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(vararg users: User)
+
+    @Delete
+    fun delete(vararg users: User)
+}
