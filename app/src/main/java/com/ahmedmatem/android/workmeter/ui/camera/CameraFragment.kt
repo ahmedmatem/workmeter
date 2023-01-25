42cdf88df92e469ebd1035ac90fcb3c81906f125
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2023-01-25 21:47:00 +0000

    Defined surfaceTextureListener

--- app/src/main/java/com/ahmedmatem/android/workmeter/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/ahmedmatem/android/workmeter/ui/camera/CameraFragment.kt
@@ -1,8 +1,10 @@
 package com.ahmedmatem.android.workmeter.ui.camera
 
+import android.graphics.SurfaceTexture
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
@@ -19,5 +21,23 @@ class CameraFragment : BaseFragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    private val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+        override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
+//            openCamera()
+        }
+
+        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+            TODO("Not yet implemented")
+        }
     }
 }
