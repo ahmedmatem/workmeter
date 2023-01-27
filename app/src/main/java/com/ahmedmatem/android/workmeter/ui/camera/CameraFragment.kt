1ec726ba0056f4de73b93e6f16843649acedccba
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2023-01-27 22:19:04 +0000

    Implement camera fragment views

--- app/src/main/java/com/ahmedmatem/android/workmeter/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/ahmedmatem/android/workmeter/ui/camera/CameraFragment.kt
@@ -1,37 +1,15 @@
 package com.ahmedmatem.android.workmeter.ui.camera
 
-import android.Manifest
-import android.content.Context
-import android.content.pm.PackageManager
-import android.graphics.ImageFormat
-import android.graphics.SurfaceTexture
-import android.hardware.camera2.CameraCharacteristics
-import android.hardware.camera2.CameraDevice
-import android.hardware.camera2.CameraManager
-import android.media.ImageReader
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.viewModels
 import com.ahmedmatem.android.workmeter.R
 import com.ahmedmatem.android.workmeter.base.BaseFragment
-import com.ahmedmatem.android.workmeter.base.BaseViewModel
 
 class CameraFragment : BaseFragment() {
     override val viewModel: CameraViewModel by viewModels()
-
-    private lateinit var cameraManager: CameraManager
-    private lateinit var cameraId: String
-    private lateinit var backgroundHandlerThread: HandlerThread
-    private lateinit var backgroundHandler: Handler
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,104 +19,5 @@ class CameraFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_camera, container, false)
     }
 
-    private val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
-        override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
-            openCamera()
-        }
 
-        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {
-            TODO("Not yet implemented")
-        }
-
-        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
-            TODO("Not yet implemented")
-        }
-
-        override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
-            TODO("Not yet implemented")
-        }
-    }
-
-    private val cameraStateCallback = object : CameraDevice.StateCallback() {
-        override fun onOpened(camera: CameraDevice) {
-            TODO("Not yet implemented")
-        }
-
-        override fun onClosed(camera: CameraDevice) {
-            super.onClosed(camera)
-        }
-
-        override fun onDisconnected(camera: CameraDevice) {
-            TODO("Not yet implemented")
-        }
-
-        override fun onError(camera: CameraDevice, error: Int) {
-            val errorMsg = when(error) {
-                ERROR_CAMERA_DEVICE -> "Fatal (device)"
-                ERROR_CAMERA_DISABLED -> "Device policy"
-                ERROR_CAMERA_IN_USE -> "Camera in use"
-                ERROR_CAMERA_SERVICE -> "Fatal (service)"
-                ERROR_MAX_CAMERAS_IN_USE -> "Maximum cameras in use"
-                else -> "Unknown"
-            }
-            Log.e("ERROR", "Error when trying to connect camera $errorMsg")
-        }
-    }
-
-    private fun openCamera() {
-        cameraManager = requireActivity().getSystemService(Context.CAMERA_SERVICE) as CameraManager
-        val cameraIds: Array<String> = cameraManager.cameraIdList
-        for (cameraId in cameraIds){
-            val cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId)
-            //If we want to choose the rear facing camera instead of the front facing one
-            if(cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) != CameraCharacteristics.LENS_FACING_BACK){
-                continue
-            }
-
-            val previewSize = cameraCharacteristics
-                .get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!
-                .getOutputSizes(ImageFormat.JPEG)
-                .maxByOrNull {
-                    it.height * it.width
-                }!!
-            val imageReader = ImageReader.newInstance(
-                previewSize.width,
-                previewSize.height,
-                ImageFormat.JPEG,
-                1
-            ).apply {
-                setOnImageAvailableListener(onImageAvailableListener, handler)
-            }
-
-            this.cameraId = cameraId
-        }
-
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.CAMERA
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        cameraManager.openCamera(cameraId, cameraStateCallback, backgroundHandler)
-    }
-
-    private fun startBackgroundThread() {
-        backgroundHandlerThread = HandlerThread("CameraVideoThread")
-        backgroundHandlerThread.start()
-        backgroundHandler = Handler(
-            backgroundHandlerThread.looper)
-    }
-
-    private fun stopBackgroundThread() {
-        backgroundHandlerThread.quitSafely()
-        backgroundHandlerThread.join()
-    }
 }
