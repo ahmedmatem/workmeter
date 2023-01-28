4b7340c6fb48bba92c1a6d32210f178326106256
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2023-01-28 17:35:46 +0000

    Request camera permission by register requestActivityResult launcher in fragment

--- app/src/main/java/com/ahmedmatem/android/workmeter/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/ahmedmatem/android/workmeter/ui/camera/CameraFragment.kt
@@ -4,10 +4,16 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
@@ -19,6 +25,15 @@ import java.util.concurrent.Executors
 
 class CameraFragment : BaseFragment() {
     override val viewModel: CameraViewModel by viewModels()
+
+    private val permissionsLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted ->
+        if(isGranted){
+            startCamera()
+        } else {
+            viewModel.showToast.value = "Permissions not granted."
+        }
+    }
 
     private lateinit var binding: FragmentCameraBinding
     private var imageCapture: ImageCapture? = null
@@ -33,8 +48,9 @@ class CameraFragment : BaseFragment() {
         if(allPermissionsGranted()){
             startCamera()
         } else {
-            ActivityCompat.requestPermissions(
-                requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+            /*ActivityCompat.requestPermissions(
+                requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)*/
+            permissionsLauncher.launch(Manifest.permission.CAMERA)
         }
 
         binding.captureButton.setOnClickListener {
@@ -46,21 +62,59 @@ class CameraFragment : BaseFragment() {
         return binding.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    /*override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        Log.d(TAG, "onRequestPermissionsResult: starts ...")
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                viewModel.showToast.value = "Permissions not granted by the user."
+            }
+        }
+    }*/
+
     private fun takePhoto() {
         TODO("Not yet implemented")
     }
 
     private fun startCamera() {
-        TODO("Not yet implemented")
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            // Used to bind lifecycle of camera to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            //Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission( requireContext(), it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraExecutor.shutdown()
     }
 
     companion object {
